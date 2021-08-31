45d59d7b95e83b6dd5c35d77613781c1907941b2
kaiquecamposdesouza <kaique.souza@aluno.faculdadeimpacta.com.br>
2021-08-30 22:23:56 -0300

    teste ggp c

---
+++ GGP_Teste_02/GGP_Teste_02/CadastroController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GGP_Teste_02
+{
+    public class CadastroController : ApiController
+    {
+        public string desc { get; set; }
+        public float valor { get; set; }
+        public int quanti { get; set; }
+
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(string desc)
+        {
+            return desc;
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
